59808c91a5f4ae91a746cc617e2f6a68a11214fb
app-jar <cofee.jar@yandex.ru>
2026-09-15 11:05:30 +0300

    Merge sort code

--- MergeSort/app/Main.hs
+++ MergeSort/app/Main.hs
@@ -1,4 +1,19 @@
 module Main where
 
+-- Сортировка слиянием
+mergeSort :: Ord a => [a] -> [a]
+mergeSort [] = []
+mergeSort [x] = [x]
+mergeSort s = let
+    (left, right) = splitAt (length s `div` 2) s
+    in merge (mergeSort left) (mergeSort right)
+  where
+    merge [] ys = ys
+    merge xs [] = xs
+    merge (x:xs) (y:ys)
+      | x <= y    = x : merge xs (y:ys)
+      | otherwise = y : merge (x:xs) ys
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    putStrLn $ show $ mergeSort [1, 2, 5, 3, 1, 4, 10, 19, 11, 23, 3, 3, 8]
